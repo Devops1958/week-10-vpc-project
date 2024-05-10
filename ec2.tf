2ae36883686aebe6a55e26b55f363ffd33b7f740
unknown <nyoyo@gmail.com>
2024-05-09 23:17:27 -0400

    project-15

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "server1" {
+resource "aws_instance" " T_server" {
   ami = "ami-0d7a109bf30624c99"
   instance_type = var.instance_Type
   vpc_security_group_ids = [aws_security_group.sg1.id ]
@@ -9,7 +9,7 @@ resource "aws_instance" "server1" {
     Name = var.enviroment
   }
 }
-resource "aws_instance" "server2" {
+resource "aws_instance" "T_server1" {
   ami = "ami-0d7a109bf30624c99"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg1.id ]
--- variable..tf
+++ variable..tf
@@ -1,9 +1,9 @@
 variable "enviroment" {
-  
+  default = "dev"
 }
 variable "Region" {
-  
+  default = "us-east-1"
 }
 variable "instance_Type" {
-  
+  default = "t2-micro"
 }
