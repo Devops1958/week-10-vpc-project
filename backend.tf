502002273c1af7d432a6ba59e0c8a341441c24ef
Devops1958 <158002456+Devops1958@users.noreply.github.com>
2024-05-09 21:32:23 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform_State_lock"
+    #dynamodb_table = "terraform_State_lock"
   }
  }
